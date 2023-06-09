d239f7f59ccd9011e5cbea286538cfa734dce0c1
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-06-09 12:15:51 +0000

    Add start script with BLDSS

--- start-ill-dev-plus.sh
+++ start-ill-dev-plus.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-DIR=/kohadevbox/koha/Koha/Illbackends/FreeForm
-if [ -d "$DIR" ];
+FFDIR=/kohadevbox/koha/Koha/Illbackends/FreeForm
+if [ -d "$FFDIR" ];
 then
-    echo "$DIR directory already exists. Skipping installation of FreeForm backend"
+    echo "$FFDIR directory already exists. Skipping installation of FreeForm backend"
 else
-    echo "Installing $DIR"
+    echo "Installing $FFDIR"
     git clone https://github.com/PTFS-Europe/koha-ill-freeform /kohadevbox/koha/Koha/Illbackends/FreeForm
 fi
+
+BLDIR=/kohadevbox/koha/Koha/Illbackends/BLDSS
+if [ -d "$BLDIR" ];
+then
+    echo "$BLDIR directory already exists. Skipping installation of BLDSS backend"
+else
+    echo "Installing $BLDIR"
+    git clone https://github.com/PTFS-Europe/koha-ill-bldss /kohadevbox/koha/Koha/Illbackends/BLDSS
+
+    echo "Installing BLDSS dependency Locale::Country"
+    sudo cpanm Locale::Country
+fi
+
 
 echo "Updating backend_directory in koha-conf.xml"
 sed -i 's/<backend_directory>\/usr\/share\/koha\/lib\/Koha\/Illbackends<\/backend_directory>/<backend_directory>\/kohadevbox\/koha\/Koha\/Illbackends<\/backend_directory> /g' /etc/koha/sites/kohadev/koha-conf.xml
